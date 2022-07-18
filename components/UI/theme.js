c650a50bbb8c18b3cdf9127ee7ec305b0097f381
gucal <gucalaliriza@outlook.com>
2022-07-18 22:51:46 +0300

    login page | ui components add

---
+++ components/UI/theme.js
@@ -0,0 +1,13 @@
+export const colors = {
+  primary: "#ff3938",
+  primaryTextColor: "#1c1e21",
+  secondaryTextColor: "#333",
+};
+
+export const fonts = {
+  sm: "0.7rem",
+  md: "0.9rem",
+  lg: "1.1rem",
+  xl: "1.5rem",
+  xxl: "2rem",
+};
---
+++ pages/login/index.tsx
@@ -0,0 +1,79 @@
+import { useContext } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+
+import AuthContext from "../../context/AuthenticationContext/store";
+import {
+  Card,
+  Container,
+  Flex,
+  Input,
+  Button,
+  FormItem,
+  FormLabel,
+  Title,
+  FormErrorLabel,
+} from "../../components/UI/style";
+
+type FormData = {
+  username: string;
+  password: string;
+};
+
+const Login: NextPage = () => {
+  const router = useRouter();
+  const { dispatch } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = handleSubmit(() => {
+    dispatch({
+      type: "LOGIN",
+    });
+    router.push(`/`);
+  });
+
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Flex style={{ marginTop: "4rem" }}>
+        <Card width={"400px"} space={3}>
+          <Title>Login</Title>
+          <form style={{ marginTop: 36 }} onSubmit={onSubmit}>
+            <FormItem>
+              <FormLabel>Username:</FormLabel>
+              <Input {...(register("username"), { required: true })} />
+              {errors.username && (
+                <FormErrorLabel>Username is required</FormErrorLabel>
+              )}
+            </FormItem>
+            <FormItem>
+              <FormLabel>Password:</FormLabel>
+              <Input
+                {...(register("password"), { required: true })}
+                type="password"
+              />
+              {errors.password && (
+                <FormErrorLabel>Password is required</FormErrorLabel>
+              )}
+            </FormItem>
+            <FormItem>
+              <Button type="submit" block>
+                Login
+              </Button>
+            </FormItem>
+          </form>
+        </Card>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Login;
