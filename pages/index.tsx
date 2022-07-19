792aab5a7fe1e0e73e89bad3eff55a038d224ff2
gucal <gucalaliriza@outlook.com>
2022-07-19 21:07:39 +0300

    userService change | login control

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,90 +1,93 @@
-import { MutableRefObject, useRef, useState, useContext } from "react";
-import type { NextPage } from "next";
-import Link from "next/link";
-import Image from "next/image";
+import { MutableRefObject, useRef, useContext, useEffect } from 'react'
+import type { NextPage } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 
-import {
-  Button,
-  Card,
-  Container,
-  Flex,
-  Input,
-  Title,
-} from "../components/UI/style";
-import Spin from "../components/Icons/Spin";
-import TwitterIcon from "../components/Icons/Twitter";
-import LinkIcon from "../components/Icons/Link";
-import UserContext from "../context/UserContext/store";
-import { getUserInfo } from "../services/users/getUserInfo";
+import { Button, Card, Container, Flex, Input, Title } from '../components/UI/style'
+import Spin from '../components/Icons/Spin'
+import TwitterIcon from '../components/Icons/Twitter'
+import LinkIcon from '../components/Icons/Link'
+import UserContext from '../context/UserContext/store'
+import { getUserInfo } from '../services/users/getUserInfo'
+import AuthContext from '../context/AuthenticationContext/store'
 
 const Home: NextPage = () => {
-  const [userLanguages, setUserLanguages] = useState<string[]>([]);
+  const { state, dispatch } = useContext(UserContext)
+  const authState = useContext(AuthContext).state
 
-  const { state, dispatch } = useContext(UserContext);
+  const isLoading: any = state.isLoading
+  const userInfo: any = state.userInfo
+  const userRepoInfo: any = state.userRepositories
+  const userTogetherLanguages: any = state.userTogetherLanguages
+  const userError: any = state.userError
 
-  const isLoading: any = state.isLoading;
-  const userInfo: any = state.userInfo;
-  const userRepoInfo: any = state.userRepositories;
+  const userNameInputRef = useRef() as MutableRefObject<HTMLInputElement>
 
-  console.log("state guncellendi", state);
-
-  const userNameInputRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const router = useRouter()
 
   const submitClick = async () => {
-    getUserInfo(userNameInputRef.current.value, dispatch);
-  };
+    await getUserInfo(userNameInputRef.current.value, dispatch)
+  }
+
+  useEffect(() => {
+    if (!authState.isLogin) {
+      router.push('/login')
+    }
+  }, [])
 
   return (
     <Container>
+      <Head>
+        <title>Github User Information</title>
+      </Head>
       <Flex gap={12}>
-        <Input block ref={userNameInputRef} placeholder="Enter username" />
-        <Button style={{ minWidth: "100px" }} onClick={submitClick}>
+        <Input
+          defaultValue={userInfo.login}
+          block
+          ref={userNameInputRef}
+          placeholder="Enter username"
+        />
+        <Button style={{ minWidth: '100px' }} onClick={submitClick}>
           Search User
         </Button>
       </Flex>
 
       {isLoading ? (
         <Spin />
+      ) : userError ? (
+        <div>Kullanici Bulunamadi</div>
       ) : (
         Object.keys(userInfo).length > 0 && (
-          <Card style={{ marginTop: "2rem" }} space={4} width={"100%"}>
-            <Flex style={{ justifyContent: "space-between" }} gap={8}>
+          <Card style={{ marginTop: '2rem' }} space={4} width={'100%'}>
+            <Flex style={{ justifyContent: 'space-between' }} gap={8}>
               <div className="flex justify-center gap-lg">
-                <Image
-                  className="rounded"
-                  width={150}
-                  height={150}
-                  src={userInfo.avatar_url}
-                />
+                <Image className="rounded" width={150} height={150} src={userInfo.avatar_url} />
                 <div className="flex column gap-md justify-center">
                   <Link href={userInfo.html_url} passHref>
-                    <a target={"_blank"}>
-                      {" "}
-                      <Title style={{ fontSize: "1.3rem" }}>
-                        {userInfo.name}
-                      </Title>
+                    <a target={'_blank'}>
+                      {' '}
+                      <Title style={{ fontSize: '1.3rem' }}>{userInfo.name}</Title>
                     </a>
                   </Link>
                   <Link href={userInfo.html_url} passHref>
-                    <a target={"_blank"}>
+                    <a target={'_blank'}>
                       <span>@{userInfo.login}</span>
                     </a>
                   </Link>
                   <span>{userInfo.location}</span>
                   <div className="flex gap-sm align-center">
                     {userInfo.twitter_username && (
-                      <Link
-                        passHref
-                        href={`https://twitter.com/${userInfo.twitter_username}`}
-                      >
-                        <a target={"_blank"}>
+                      <Link passHref href={`https://twitter.com/${userInfo.twitter_username}`}>
+                        <a target={'_blank'}>
                           <TwitterIcon />
                         </a>
                       </Link>
                     )}
                     {userInfo.blog && (
                       <Link passHref href={`${userInfo.blog}`}>
-                        <a target={"_blank"}>
+                        <a target={'_blank'}>
                           <LinkIcon />
                         </a>
                       </Link>
@@ -95,28 +98,26 @@ const Home: NextPage = () => {
 
               <div className="flex column gap-sm justify-center">
                 <span>
-                  Followers:{" "}
-                  <span className="text-bold">{userInfo.followers}</span>
+                  Followers: <span className="text-bold">{userInfo.followers}</span>
                 </span>
                 <span>
-                  Following:{" "}
-                  <span className="text-bold">{userInfo.following}</span>
+                  Following: <span className="text-bold">{userInfo.following}</span>
                 </span>
                 <span>
-                  Public Repository:{" "}
+                  Public Repository:{' '}
                   <span className="text-bold">
-                    {userRepoInfo.length >= 30 ? "30+" : userRepoInfo.length}
+                    {userRepoInfo.length >= 30 ? '30+' : userRepoInfo.length}
                   </span>
                 </span>
               </div>
             </Flex>
-            <hr style={{ margin: "2rem 0" }} />
+            <hr style={{ margin: '2rem 0' }} />
             <Flex>
               <div className="grid-container">
-                {userLanguages.map((item: any) => (
+                {userTogetherLanguages.map((item: any) => (
                   <div className="grid-item">
                     <span className="text-bold">{item.name}</span>
-                    <span style={{ fontSize: ".9rem" }}>%{item.size}</span>
+                    <span style={{ fontSize: '.9rem' }}>%{item.size}</span>
                   </div>
                 ))}
               </div>
@@ -125,7 +126,7 @@ const Home: NextPage = () => {
         )
       )}
     </Container>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- services/users/getUserInfo.ts
+++ services/users/getUserInfo.ts
@@ -1,81 +1,81 @@
-import { getAction } from "../config";
+import { getAction } from '../config'
 
-export const getUserInfo = (
-  userName: string,
-  dispatch: React.Dispatch<any>
-) => {
+export const getUserInfo = async (userName: string, dispatch: React.Dispatch<any>) => {
   dispatch({
-    type: "PENDING",
-  });
-  getAction(`/users/${userName}`).then((res: any) => {
+    type: 'PENDING',
+  })
+  try {
+    let response: any = await getAction(`/users/${userName}`)
     dispatch({
-      type: "SET_USER_INFO",
-      payload: res.data,
-    });
-    getUserRepos(userName, res.data, dispatch);
-  });
-};
-
-
-//TODO: Ayrilacak.
-const getUserRepos = async (
-  userName: string,
-  data: any,
-  dispatch: React.Dispatch<any>
-) => {
-  getAction(`/users/${data.login}/repos`).then((res: any) => {
+      type: 'SET_USER_INFO',
+      payload: response.data,
+    })
+    getUserRepos(response.data, dispatch)
+  } catch (error) {
     dispatch({
-      type: "SET_USER_REPOSITORIES",
-      payload: res.data,
-    });
-    let items: any = [];
-    for (let i = 0; i < res.data.length; i++) {
-      getAction(`/repos/${userName}/${res.data[i].name}/languages`).then(
-        (res: any) => {
-          for (const [key, value] of Object.entries(res.data)) {
-            let langInfo = {
-              name: key,
-              size: value,
-            };
-            items.push(langInfo);
-          }
-          dispatch({
-            type: "SET_USER_ALL_LANGUAGES",
-            payload: items,
-          });
-        }
-      );
-      let langsGroup = items.reduce(function (r: any, a: any) {
-        console.log(a, r);
-        r[a.name] = r[a.name] || [];
-        r[a.name].push(a);
-        return r;
-      }, Object.create(null));
+      type: 'USER_NOT_DEFINED',
+      payload: error,
+    })
+  }
+}
 
-      console.log(langsGroup);
+const getUserRepos = async (data: any, dispatch: React.Dispatch<any>) => {
+  let response: any = await getAction(`/users/${data.login}/repos`)
+  dispatch({
+    type: 'SET_USER_REPOSITORIES',
+    payload: response.data,
+  })
+  getUserLanguages(data.login, response.data, dispatch)
+}
 
-      let rates: any = [];
-      Object.keys(langsGroup).map((item) => {
-        let codeSum = 0;
-        for (let j = 0; j < langsGroup[item].length; j++) {
-          codeSum += langsGroup[item][j].size;
-        }
-        rates.push({ name: item, size: codeSum });
-      });
+const getUserLanguages = async (userName: string, repos: any, dispatch: React.Dispatch<any>) => {
+  let languages: object[] = []
 
-      let totalRate: number = 0;
+  for (let i = 0; i < repos.length; i++) {
+    let res: any = await getAction(`/repos/${userName}/${repos[i].name}/languages`)
 
-      for (let index = 0; index < rates.length; index++) {
-        totalRate += rates[index].size;
+    for (const [key, value] of Object.entries(res.data)) {
+      let langInfo = {
+        name: key,
+        size: value,
       }
-
-      for (let index = 0; index < rates.length; index++) {
-        rates[index].size = ((100 * rates[index].size) / totalRate).toFixed(2);
-      }
-      console.log("llll", langsGroup);
+      languages.push(langInfo)
     }
-  });
+    dispatch({
+      type: 'SET_USER_ALL_LANGUAGES',
+      payload: languages,
+    })
+
+    let langsGroup = languages.reduce(function (pre: any, curr: any) {
+      pre[curr.name] = pre[curr.name] || []
+      pre[curr.name].push(curr)
+      return pre
+    }, Object.create(null))
+
+    let rates: any = []
+    Object.keys(langsGroup).map((item) => {
+      let codeSum = 0
+      for (let j = 0; j < langsGroup[item].length; j++) {
+        codeSum += langsGroup[item][j].size
+      }
+      rates.push({ name: item, size: codeSum })
+    })
+
+    let totalRate: number = 0
+    for (let index = 0; index < rates.length; index++) {
+      totalRate += rates[index].size
+    }
+
+    for (let index = 0; index < rates.length; index++) {
+      rates[index].size = ((100 * rates[index].size) / totalRate).toFixed(2)
+    }
+
+    dispatch({
+      type: 'SET_USER_TOGETHER_LANGUAGES',
+      payload: rates,
+    })
+  }
   dispatch({
-    type: "COMPLETED",
-  });
-};
+    type: 'COMPLETED',
+  })
+}
