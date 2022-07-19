f1ae079cb88914d0ecfa25e8468c27a4fab26021
gucal <gucalaliriza@outlook.com>
2022-07-19 21:09:13 +0300

    reducer changes

--- context/UserContext/reducer.tsx
+++ context/UserContext/reducer.tsx
@@ -1,21 +1,34 @@
+import { initialState } from './store'
+
 export function reducer(state: any, action: any) {
   switch (action.type) {
-    case "PENDING":
-      state.isLoading = true;
-      return { ...state };
-    case "SET_USER_INFO":
-      state.userInfo = action.payload;
-      return { ...state };
-    case "SET_USER_REPOSITORIES":
-      state.userRepositories = action.payload;
-      return { ...state };
-    case "SET_USER_ALL_LANGUAGES":
-      state.userAllLanguages = action.payload;
-      return { ...state };
-    case "COMPLETED":
-      state.isLoading = false;
-      return { ...state };
+    case 'PENDING':
+      state.userError = false
+      state.isLoading = true
+      return { ...state }
+    case 'SET_USER_INFO':
+      state.userInfo = action.payload
+      return { ...state }
+    case 'SET_USER_REPOSITORIES':
+      state.userRepositories = action.payload
+      return { ...state }
+    case 'SET_USER_ALL_LANGUAGES':
+      state.userAllLanguages = action.payload
+      return { ...state }
+    case 'SET_USER_TOGETHER_LANGUAGES':
+      state.userTogetherLanguages = action.payload
+      return { ...state }
+    case 'USER_NOT_DEFINED':
+      state.userError = true
+      state.isLoading = false
+      return { ...state }
+    case 'COMPLETED':
+      state.isLoading = false
+      return { ...state }
+    case 'RESET':
+      state = initialState
+      return { ...state }
     default:
-      return state;
+      return state
   }
 }
